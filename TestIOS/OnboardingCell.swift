33f470bc2bc754242f97fc943b919f4354fad88f
nikkogolenok <nikiton17ns@gmail.com>
2026-01-28 13:12:14 +0300

    In OnboardingViewController add collectionView and continueButton. Create custom cell for collection OnboardingCell

---
+++ TestIOS/OnboardingCell.swift
@@ -0,0 +1,90 @@
+//
+//  OnboardingCell.swift
+//  TestIOS
+//
+//  Created by Никита Коголенок on 28.01.26.
+//
+
+import UIKit
+
+final class OnboardingCell: UICollectionViewCell {
+    
+    // MARK: - Variable
+    static let identifier = "OnboardingCell"
+    
+    // MARK: -  GUI Variable
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "clock")
+        image.contentMode = .scaleAspectFit
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    private let titleLable: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Title"
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription"
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    // MARK: - View LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setupView() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(imageView)
+        containerView.addSubview(descriptionLabel)
+        containerView.addSubview(titleLable)
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            imageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor, constant: -50),
+            imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.6),
+            imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.6),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 32),
+            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -32),
+            
+            titleLable.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            titleLable.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            titleLable.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+        ])
+    }
+}
--- TestIOS/OnboardingViewController.swift
+++ TestIOS/OnboardingViewController.swift
@@ -7,6 +7,84 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+final class OnboardingViewController: UIViewController {
     
+    // MARK: - GUI Variables
+    private var collectionView: UICollectionView!
+    private var continueButton: UIButton!
+    
+    // MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectioView()
+        setupContinueButton()
+    }
+    
+    // MARK: - Methods
+    private func setupCollectioView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.clipsToBounds = true
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(OnboardingCell.self, forCellWithReuseIdentifier: OnboardingCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    private func setupContinueButton() {
+        continueButton = UIButton(type: .system)
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        continueButton.setTitleColor(.red, for: .normal)
+        continueButton.backgroundColor = .systemBlue
+        continueButton.layer.cornerRadius = 16
+        continueButton.addTarget(self, action: #selector(continueAction), for: .touchUpInside)
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(continueButton)
+        
+        NSLayoutConstraint.activate([
+            continueButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            continueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            continueButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
+        ])
+    }
+    
+    // MARK: - Action
+    @objc private func continueAction() {
+        print("continueAction")
+    }
+}
+
+// MARK: - OnboardingViewController: UICollectionViewDelegate
+extension OnboardingViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+}
+
+// MARK: -
+extension OnboardingViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCell.identifier, for: indexPath)
+        return cell
+    }
 }
