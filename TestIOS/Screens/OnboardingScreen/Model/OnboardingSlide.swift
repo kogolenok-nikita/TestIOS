393c9f503d55f3ce6fd4e790b41aecea9f701240
nikkogolenok <nikiton17ns@gmail.com>
2026-01-28 13:20:00 +0300

    Create model OnboardingSlide for Onboarding

---
+++ TestIOS/Screens/OnboardingScreen/Model/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  TestIOS
+//
+//  Created by Никита Коголенок on 28.01.26.
+//
+
+import Foundation
+
+struct OnboardingSlide {
+    let imageName: String
+    let title: String
+    let description: String
+}
